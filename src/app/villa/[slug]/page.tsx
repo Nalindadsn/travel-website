c941135346bcf4c02caa181863feb5255a9b3a11
Nalindadsn <nalindadsn@gmail.com>
2025-01-23 02:13:29 +0530

    add files1

--- src/app/villa/[slug]/page.tsx
+++ src/app/villa/[slug]/page.tsx
@@ -3,6 +3,7 @@
 export default async function Post({ params, searchParams }: any) {
   const slug = (await params).slug;
   console.log(slug);
+  console.log(searchParams);
 
   return <div></div>;
 }
