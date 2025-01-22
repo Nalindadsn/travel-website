698eb01beea748a2b33a0d94890c9ca283db4f4d
Nalindadsn <nalindadsn@gmail.com>
2025-01-23 01:35:17 +0530

    add files

--- src/components/DetailsSlider.tsx
+++ src/components/DetailsSlider.tsx
@@ -2,11 +2,12 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
-function DetailsSlider({ images }: { images: any }) {
+function DetailsSlider({ images }: { images: string[] }) {
   const [active, setActive] = useState(0);
 
   return (
     <div className=" rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+      {JSON.stringify(images)}
       <Image
         className="w-full h-full object-cover"
         src={images[active]}
@@ -15,7 +16,7 @@ function DetailsSlider({ images }: { images: any }) {
         height={500}
       />
       <div className="flex w-full gap-2 mt-2">
-        {images.map((x: string | any) => (
+        {images.map((x: string) => (
           <div
             key={`${x}-${images.indexOf(x)}`}
             className="w-1/4 cursor-pointer"
