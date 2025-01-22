e0e1dd6242647f76b3e1c46d697ae80ef79f0bc6
Nalindadsn <nalindadsn@gmail.com>
2025-01-23 02:01:20 +0530

    add files1

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
+import type { NextConfig } from "next";
 import { FlatCompat } from "@eslint/eslintrc";
-const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-});
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next"],
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  }),
-];
-export default eslintConfig;
+
+const nextConfig: NextConfig = {
+  /* config options here */ rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
+
+export default nextConfig;
