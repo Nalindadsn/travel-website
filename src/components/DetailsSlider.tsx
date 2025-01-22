1498763e7b4902b462729fa43e37c8955da1464a
Nalindadsn <nalindadsn@gmail.com>
2025-01-23 01:30:49 +0530

    add files

--- src/components/DetailsSlider.tsx
+++ src/components/DetailsSlider.tsx
@@ -15,7 +15,7 @@ function DetailsSlider({ images }: { images: any }) {
         height={500}
       />
       <div className="flex w-full gap-2 mt-2">
-        {images.map((x) => (
+        {images.map((x: string | any) => (
           <div
             key={`${x}-${images.indexOf(x)}`}
             className="w-1/4 cursor-pointer"
