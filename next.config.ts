8a62373a65df8090880ce8d3c0bb40176ab7eaa5
Nalindadsn <nalindadsn@gmail.com>
2025-01-23 01:56:46 +0530

    add files

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-  },
-};
-
-export default nextConfig;
+import { FlatCompat } from "@eslint/eslintrc";
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+});
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next", "prettier"],
+  }),
+];
+export default eslintConfig;
